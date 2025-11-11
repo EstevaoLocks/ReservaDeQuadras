5bf485c296a953cc125e5b2250698251aa0c4ef2
EstevaoLocks <estevao.locks11@gmail.com>
2025-11-11 14:08:02 -0300

    Add reader

--- index.php
+++ index.php
@@ -40,6 +40,32 @@
                 <a class="btn-secondary1 union" href="#">Login</a>
             </div>
         </div>
-    </nav>
+    </nav><!-- Fim Menu -->
+
+    <!-- Conteúdo Principal ----------- -->
+
+    <!-- Cabeçalho Home -->
+    <header class="home">
+        <!-- Gradiente -->
+        <div class="gradiente">
+            <!-- Conteúdo Header -->
+            <div class="conteudo-header">
+                <!-- Textos -->
+                <h1>Quem <code>espera</code> a bola vir no pé, <code>perde</code> o <code>chute</code></h1>
+                <p>Reserve ou cadastre sua quadra já!</p>
+
+                <!-- Botões -->
+                <div class="btns-header">
+                    <a class="btn-primary2" href="#">Cadastre-se</a>
+                    <a class="btn-primary1" href="#">Login</a>
+                </div>
+            </div>
+
+            <!-- Figura -->
+            <div class="figura">
+                <img src="assets/img/figuraMouse.png" alt="Icone de mouse">
+            </div>
+        </div>
+    </header>
 </body>
 </html>
